1287ad496999f3b3f71f54c749c310bb2909ec86
Travis Pessetto <travis@pessetto.com>
2014-09-11 15:14:36 -0600

    Fix readme

--- lib/hamburger.rb
+++ lib/hamburger.rb
@@ -3,9 +3,5 @@ require "railties/hamburger.rb"
 require "modules/backupable.rb"
 
 module Hamburger
-  class Railtie < Rails::Railtie
-  	initialize "hamburger.backupable" do
-		ActiveRecord::Base.send :include, Backupable
-	end
-  end
+
 end
--- lib/railties/hamburger.rb
+++ lib/railties/hamburger.rb
@@ -1,7 +1,15 @@
-class HamburgerRailtie < Rails::Railtie
-	rake_tasks do
-		Dir[File.join(File.dirname(__FILE__),'../tasks/')+"*.rb"].each do |file|
-			load file
+module HamburgerRails
+	class HamburgerRailtie < Rails::Railtie
+
+		initializer "hamburger_railtie.backupable" do
+			ActiveRecord::Base.send :include, Backupable
 		end
+	
+		rake_tasks do
+			Dir[File.join(File.dirname(__FILE__),'../tasks/')+"*.rb"].each do |file|
+				load file
+			end
+		end
+	
 	end
 end
